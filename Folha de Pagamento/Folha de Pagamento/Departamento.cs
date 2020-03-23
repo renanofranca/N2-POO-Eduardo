0d4e40e90fc818e4e20413125b9724f617945d4f
HenriqueGalli-atomic <henrique.galli@atomicsolutions.com.br>
2020-03-23 20:29:37 -0300

    Inicio do desenvolvilmento da classe funcionario

---
+++ Folha de Pagamento/Folha de Pagamento/Departamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folha_de_Pagamento
+{
+    class Departamento
+    {
+        private int codigo;
+        private string descricao;
+        private int codigoGerente;
+
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public int CodigoGerente { get; set; }
+    }
+}
---
+++ Folha de Pagamento/Folha de Pagamento/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Folha_de_Pagamento
+{
+    class Funcionario
+    {
+        private int codigo;
+        private string nome;
+        private string cpf;
+        private DateTime dataNascimento;
+        private double salario;
+        private int departamento;
+        private char tipo;
+
+        public string Nome
+        {
+            get => nome;
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    
+                }
+
+            }
+        }
+    }
+}
