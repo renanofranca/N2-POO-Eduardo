eb0ad19536b35289d415f1f4ff843866648d7015
HenriqueGalli-atomic <henrique.galli@atomicsolutions.com.br>
2020-03-24 20:45:55 -0300

    Inicio do desenvolvimento do cadastro

--- Folha de Pagamento/Folha de Pagamento/Form1.cs
+++ Folha de Pagamento/Folha de Pagamento/Form1.cs
@@ -15,6 +15,53 @@ namespace Folha_de_Pagamento
         public Form1()
         {
             InitializeComponent();
+            
+            //Necessario carregar a cbxDepartamentos do cadastro de funcionarios com os 
+            //departamentos cadastrados
+        }
+        List<Funcionario> funcionarios = new List<Funcionario>();
+        private void Função_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cadastro_Click(object sender, EventArgs e)
+        {
+            Funcionario f;
+
+            try
+            {
+                int codigo = Convert.ToInt16(txtCodigo.Value);
+                string nome = txtNomeFuncionario.Text;
+                string cPF = txtCPF.Text;
+                DateTime dataNascimento = Convert.ToDateTime(txtNascimento.Text);
+                double salario = Convert.ToDouble(txtSalario.Text);
+
+                char tipo = ' ';
+                if (btnSelectFuncionario.Checked)
+                    tipo = 'F';
+                else if (btnSelectGerente.Checked)
+                    tipo = 'G';
+
+                int departamento = 1;
+                if (btnSelectFuncionario.Checked)
+                {
+                    //aqui a cbxDepartamento precisa ter algum valor (nao obrigatorio para gerentes)
+                }
+                f = new Funcionario(codigo, nome, cPF, dataNascimento, salario, departamento, tipo);
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Informe valores de cadastro válidos", "Cadastro", MessageBoxButtons.OK);
+                return;
+            }
+            funcionarios.Add(f);
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Folha de Pagamento/Folha de Pagamento/Funcionario.cs
+++ Folha de Pagamento/Folha de Pagamento/Funcionario.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +10,26 @@ namespace Folha_de_Pagamento
 {
     class Funcionario
     {
-        private int codigo;
+        
         private string nome;
         private string cpf;
         private DateTime dataNascimento;
         private double salario;
-        private int departamento;
         private char tipo;
 
+        public Funcionario(int _codigo,string _nome, string _cpf, DateTime _dataNascimento, double _salario, int _departamento, char _tipo)
+        {
+            Codigo = _codigo;
+            Nome = _nome;
+            CPF = _cpf;
+            DataNascimento = _dataNascimento;
+            Salario = _salario;
+            Departamento = _departamento;
+            Tipo = _tipo;
+        }
+
+        public int Codigo { get; set; }
+        public int Departamento { get; set; }
         public string Nome
         {
             get => nome;
@@ -24,10 +37,68 @@ namespace Folha_de_Pagamento
             {
                 if (String.IsNullOrEmpty(value))
                 {
-                    
+                    MessageBox.Show("Nome obrigatório");
+                    return;
                 }
 
+                nome = value;
             }
         }
+        public string CPF
+        {
+            get => cpf;
+            set
+            {
+                if(value.Length != 14)
+                {
+                    MessageBox.Show("CPF inválido");
+                    return;
+                }
+                cpf = value;
+            }
+        }
+
+        public DateTime DataNascimento
+        {
+            get => dataNascimento;
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    MessageBox.Show("Data Nascimento inválida");
+                    return;               
+                }
+                    dataNascimento = value;
+            }
+        }
+        public double Salario
+        {
+            get => salario;
+            set
+            {
+                if (value <= 0)
+                {
+                    MessageBox.Show("Salário inválido");
+                    return;
+                }            
+                salario = value;
+            }
+        }
+        public char Tipo
+        {
+            get => tipo;
+            set
+            {
+                if(value != 'G' && value != 'F')
+                {
+                    MessageBox.Show("Função Inválida");
+                    return;
+                }
+                   
+                tipo = value;
+            }
+        }
+
+        
     }
 }
